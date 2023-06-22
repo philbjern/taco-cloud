43c3acfc0add165f37d1a4b211a3b708c86c6b8a
archloner <filipbiernat@gmail.com>
2023-06-22 15:11:31 +0200

    Application changes for using Spring Data JPA

--- src/main/java/sia/tacos/model/Taco.java
+++ src/main/java/sia/tacos/model/Taco.java
@@ -1,5 +1,6 @@
 package sia.tacos.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,8 +9,11 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,8 +22,13 @@ public class Taco {
     @Size(min=5, message="Nazwa musi składać się z przynajmniej pięciu znaków.")
     private String name;
 
-    @NotNull(message = "Musisz wybrać przynajmniej jeden składnik.")
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1, message="Musisz wybrać przynajmniej jeden składnik.")
     private List<String> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 
 }
