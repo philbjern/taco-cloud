83f16a42e49d3f480a84d1ae08b782bfe697dcb8
fbiernat <filipbiernat@gmail.com>
2023-07-13 14:35:27 +0200

    Update SecurityConfig to allow access to h2-console

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package tacos.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,15 +32,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .authorizeHttpRequests()
                     .requestMatchers("/design", "/orders").hasRole("USER")
+                    .requestMatchers("/h2-console/**").permitAll()
                     .requestMatchers("/", "/**").permitAll()
                 .and()
-                    .formLogin()
-                    .loginPage("/login")
+                    .headers().frameOptions().disable()
                 .and()
-                .build();
+                    .formLogin()
+                    .loginPage("/login");
+        http.csrf().disable();
+        return http.build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring()
+                    .requestMatchers("/h2-console/**");
+        };
+    }
+
 
 }
