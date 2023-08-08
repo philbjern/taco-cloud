beaa14b560b81f36704f5b77d569bed78b3ae316
fbiernat <filipbiernat@gmail.com>
2023-08-08 15:44:59 +0200

    Add REST controllers code

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -36,4 +36,10 @@ public class TacoController {
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
 }
