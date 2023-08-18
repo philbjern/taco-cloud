4b74a11b65665960b1b2fa104be9fbe2a1697783
fbiernat <filipbiernat@gmail.com>
2023-08-18 15:42:11 +0200

    Application changes at the end of chapter 8 securing rest

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,47 +17,90 @@ import tacos.data.UserRepository;
 import tacos.model.User;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers(HttpMethod.POST, "/api/ingredients")
+                  .hasAuthority("SCOPE_writeIngredients")
+                .antMatchers(HttpMethod.DELETE, "/api/ingredients")
+                  .hasAuthority("SCOPE_deleteIngredients")
+                .antMatchers("/api//tacos", "/api//orders/**")
+                  .permitAll()
+                .antMatchers("/**")
+                  .permitAll()
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+
+                .httpBasic()
+                  .realmName("Taco Cloud")
+
+                .and()
+                  .logout()
+                  .logoutSuccessUrl("/")
+
+                .and()
+                  .csrf()
+                    .ignoringAntMatchers("/h2-console/**", "/api/**")
+
+                .and()
+                .headers()
+                  .frameOptions()
+                    .sameOrigin();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepo) {
-        return username -> {
-            User user = userRepo.findByUsername(username);
-            if (user != null) return user;
-
-            throw new UsernameNotFoundException("User '" + username + "' not found");
-        };
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeHttpRequest -> {
-            authorizeHttpRequest.antMatchers("/design", "/orders").hasRole("USER")
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/", "/**").permitAll();
-        });
-        http.headers().frameOptions().disable()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                .and()
-                    .logout()
-                    .logoutSuccessUrl("/");
-        http.csrf().disable();
-        return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> {
-            web.ignoring()
-                    .antMatchers("/h2-console/**");
-        };
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(UserRepository userRepo) {
+//        return username -> {
+//            User user = userRepo.findByUsername(username);
+//            if (user != null) return user;
+//
+//            throw new UsernameNotFoundException("User '" + username + "' not found");
+//        };
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(authorizeHttpRequest -> {
+//            authorizeHttpRequest.antMatchers("/design", "/orders").hasRole("USER")
+//                .antMatchers("/h2-console/**").permitAll()
+//                .antMatchers("/", "/**").permitAll();
+//        });
+//        http.headers().frameOptions().disable()
+//                .and()
+//                    .formLogin()
+//                    .loginPage("/login")
+//                .and()
+//                    .logout()
+//                    .logoutSuccessUrl("/");
+//        http.csrf().disable();
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> {
+//            web.ignoring()
+//                    .antMatchers("/h2-console/**");
+//        };
+//    }
 
 }
