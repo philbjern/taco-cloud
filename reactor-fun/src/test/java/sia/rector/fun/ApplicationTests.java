1ef669e9c3b904abc569c921db209bcf9d731828
archloner <filipbiernat@gmail.com>
2023-11-10 19:51:35 +0100

    Add new test case

--- reactor-fun/src/test/java/sia/rector/fun/ApplicationTests.java
+++ reactor-fun/src/test/java/sia/rector/fun/ApplicationTests.java
@@ -3,6 +3,7 @@ package sia.rector.fun;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class ApplicationTests {
@@ -13,10 +14,28 @@ class ApplicationTests {
 
 	@Test
 	void createAFlux_just() {
-		Flux<String> fruitFlux = Flux.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+		Flux<String> fruitFlux = Flux
+				.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
 		fruitFlux.subscribe(
 				f -> System.out.println("Here's some fruit: " + f)
 		);
+	}
+
+	@Test
+	void createFlux_fromArrray() {
+		String[] fruits = new String[] {
+				"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+
+		Flux<String> fruitFlux = Flux.fromArray(fruits);
+
+		StepVerifier.create(fruitFlux)
+				.expectNext("Apple")
+				.expectNext("Orange")
+				.expectNext("Grape")
+				.expectNext("Banana")
+				.expectNext("Strawberry")
+				.verifyComplete();
 	}
 
 }
