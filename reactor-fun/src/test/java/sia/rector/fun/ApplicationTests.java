a60ca0a8d37a869456b9165c2189bd87b563aa70
archloner <filipbiernat@gmail.com>
2023-12-28 18:45:22 +0100

    Reactor changes

--- reactor-fun/src/test/java/sia/rector/fun/ApplicationTests.java
+++ reactor-fun/src/test/java/sia/rector/fun/ApplicationTests.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class ApplicationTests {
@@ -22,14 +28,8 @@ class ApplicationTests {
 		);
 	}
 
-	@Test
-	void createFlux_fromArrray() {
-		String[] fruits = new String[] {
-				"Apple", "Orange", "Grape", "Banana", "Strawberry"};
-
-		Flux<String> fruitFlux = Flux.fromArray(fruits);
-
-		StepVerifier.create(fruitFlux)
+	void verifyFruitFlux(Flux<String> flux) {
+		StepVerifier.create(flux)
 				.expectNext("Apple")
 				.expectNext("Orange")
 				.expectNext("Grape")
@@ -38,4 +38,108 @@ class ApplicationTests {
 				.verifyComplete();
 	}
 
+	@Test
+	void createFlux_fromArrray() {
+		String[] fruits = new String[] {
+				"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+
+		Flux<String> fruitFlux = Flux.fromArray(fruits);
+
+		verifyFruitFlux(fruitFlux);
+	}
+
+	@Test
+	void createFlux_fromIterable() {
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("Apple");
+		fruitList.add("Orange");
+		fruitList.add("Grape");
+		fruitList.add("Banana");
+		fruitList.add("Strawberry");
+
+		Flux<String> fruitFlux = Flux.fromIterable(fruitList);
+
+		verifyFruitFlux(fruitFlux);
+	}
+
+	@Test
+	void createFlux_fromStream() {
+		Stream<String> fruitStream = Stream.of("Apple", "Orange", "Grape",
+				"Banana", "Strawberry");
+
+		Flux<String> fruitFlux = Flux.fromStream(fruitStream);
+
+		verifyFruitFlux(fruitFlux);
+	}
+
+	@Test
+	void createFlux_range() {
+		Flux<Integer> rangeFlux = Flux.range(1, 5);
+
+		StepVerifier.create(rangeFlux)
+				.expectNext(1)
+				.expectNext(2)
+				.expectNext(3)
+				.expectNext(4)
+				.expectNext(5)
+				.verifyComplete();
+	}
+
+	@Test
+	void createFlux_interval() {
+		Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1))
+				.take(5);
+
+		StepVerifier.create(intervalFlux)
+				.expectNext(0L)
+				.expectNext(1L)
+				.expectNext(2L)
+				.expectNext(3L)
+				.expectNext(4L)
+				.verifyComplete();
+	}
+
+	@Test
+	void mergeFluxes() {
+		Flux<String> characterFlux = Flux
+				.just("Garfield", "Kojak", "Barbossa")
+				.delayElements(Duration.ofMillis(500));
+
+		Flux<String> foodFlux = Flux
+				.just("Lasagna", "Lollipops", "Apples")
+				.delaySubscription(Duration.ofMillis(250))
+				.delayElements(Duration.ofMillis(500));
+
+		Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+		StepVerifier.create(mergedFlux)
+				.expectNext("Garfield")
+				.expectNext("Lasagna")
+				.expectNext("Kojak")
+				.expectNext("Lollipops")
+				.expectNext("Barbossa")
+				.expectNext("Apples")
+				.verifyComplete();
+	}
+
+	@Test
+	void zipFluxes() {
+		Flux<String> characterFlux = Flux
+				.just("Garfield", "Kojak", "Barbossa");
+
+		Flux<String> foodFlux = Flux
+				.just("Lasagna", "Lollipops", "Apples");
+
+		Flux<Tuple2<String, String>> zippedFlux =
+				Flux.zip(characterFlux, foodFlux);
+
+		StepVerifier.create(zippedFlux)
+				.expectNextMatches(p ->
+					p.getT1().equals("Garfield") && p.getT2().equals("Lasagna"))
+				.expectNextMatches(p ->
+					p.getT1().equals("Kojak") && p.getT2().equals("Lollipops"))
+				.expectNextMatches(p ->
+					p.getT1().equals("Barbossa") && p.getT2().equals("Apples"))
+				.verifyComplete();
+	}
 }
